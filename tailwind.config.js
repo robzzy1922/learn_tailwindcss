281fb9cd50a5027beebe89b253f6a25f43e8d517
robzdd1922 <robipermanaofficial@gmail.com>
2024-08-28 11:01:31 +0700

    learn

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        biasa: "000000",
+      },
+    },
   },
   plugins: [],
 };
